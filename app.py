0eb1536aa50d458a37106d40304340d5193b83e4
Sadanand <sadanand9595@gmail.com>
2026-05-15 11:36:31 +0530

    Final Railway fix

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask app running successfully on Railway"
+    return render_template("index.html")
+
+@app.route("/generate")
+def generate():
+    return render_template("generate_content.html")
+
+@app.route("/history")
+def history():
+    return render_template("content_history.html")
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
